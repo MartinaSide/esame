7c902c92e43593d29113b91b83e0d6adefb09cb7
Martina Siderini <93184296+MartinaSide@users.noreply.github.com>
2022-02-04 18:08:01 +0000

    aggiunto datum, is_data a get_data()

--- main.py
+++ main.py
@@ -30,15 +30,22 @@ class CSVTimeSeriesFile:
                 # Con la funzione strip() tolgo il carattere di new line:
                 elements[-1] = elements[-1].strip()
 
-                datum = elements[0].split('-')
+                #datum è una lista contentente la prima parte di elements, cioè la data
+                datum = 0
+                #provo a dividere sulla "-"
+                try:
+                    datum = elements[0].split('-')
+                #quando non è presente, l'elemento che sto analizzando non è una data nel formato richiesto e quindi lo ignoro o è la riga di intestazione e quindi la ignoro
+                except:
+                    datum = [0, 0]
                 is_data = True
                 if datum[0] not in range [1949, 1960]:
                     is_data = False
                 if datum[1] not in range [1, 12]:
                     is_data = False
     
-                # Se NON sto processando l'intestazione:
-                if elements[0] != 'Date' or 'date':
+                # is_data mi permette di controllare se si tratta dell'intestazione, di righe fuori dal range o se si tratta di pezzi di testo che non c'entrano nulla
+                if is_data is True:
                     #se il valore non è nullo o negativo ed è un intero
                     if elements[1] > 0 and elements[1] is int:
                         if elements[0] > last_date:
