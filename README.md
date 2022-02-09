d4c207673d6fa43af37fadfb18c8902988336a53
Martina Siderini <93184296+MartinaSide@users.noreply.github.com>
2022-02-09 21:26:36 +0000

    versione definitiva

--- README.md
+++
@@ -1,9 +0,0 @@
-Parti da integrare ed errori da gestire:
-- potrebbe non essere possibile convertire in int elements[1]
-- potrebbe non essere possibile convertire in int datum[0] e datum[1]
-
-Parti da implementare:
-- funzione compute_avg_monthly_difference
-
-Cose da scoprire come fare:
-- Accedere ad un elemento di un elemento di una lista
--- esame.py
+++ esame.py
@@ -4,6 +4,7 @@ class ExamException(Exception):
 
 class CSVTimeSeriesFile:
     def __init__(self, name):
+        #name è il nome del file
         self.name = name
 
     def get_data(self):
@@ -11,14 +12,17 @@ class CSVTimeSeriesFile:
         try:
             my_file = open(self.name, 'r')
             my_file.readline()
+        #se non riesco ad aprire il file, perché non lo trovo, alzo un'eccezione:
         except:
             raise ExamException("Errore in apertura del file {}".format(
                 self.name))
-            # Inizializzo una lista vuota per salvare tutti i dati
+        
+        # Inizializzo una lista vuota per salvare tutti i dati
         time_series = []
-        # last_date è il mese precedente al primo che mi potrebbe venir assegnato
-        # mi serve per verificare che l'input sia ordinato e non contenga righe doppie
+
+        # last_date è il mese precedente al primo che mi potrebbe venir assegnato mi serve per verificare che l'input sia ordinato e non contenga righe doppie
         last_date = "1948-12"
+
         # Apro il file
         my_file = open(self.name, 'r')
 
@@ -31,13 +35,14 @@ class CSVTimeSeriesFile:
                 is_data = False
                 is_int = True
                 if len(elements) < 2:
+
                     #non lavoro su questi dati e li ignoro
                     is_data = False
                     is_int = False
                 elif len(elements) == 2:
+
                     # Con la funzione strip() tolgo il carattere di new line:
                     elements[1] = elements[1].strip()
-                    #print (elements[1])
                 else:
                     while len(elements)>2:
                         elements.pop(-1)
@@ -45,38 +50,49 @@ class CSVTimeSeriesFile:
                 
                 #datum è una lista contentente la prima parte di elements, cioè la data
                 datum = [0, 0]
-                #provo a dividere sulla "-"
+                
+                #provo a dividere sulla "-" che so essere l'elemento
                 if "-" in elements[0]:
                     datum = elements[0].split('-')
+                    
                     #quando non è presente, l'elemento che sto analizzando non è una data nel formato richiesto e quindi lo ignoro o è la riga di intestazione e quindi la ignoro
                     is_data = True
+                    
                     datum[0] = int(datum[0])
+                    
                     datum[1] = int(datum[1])
+                    
                     if datum[0] not in range(1949, 1960):
                         is_data = False
+                    
                     if datum[1] not in range(1, 13):
                         is_data = False
-                    # is_data mi permette di controllare se si tratta dell'intestazione, di righe fuori dal range o se si tratta di pezzi di testo che non c'entrano nulla
-                    #print (is_data)
+                    
+                # is_data mi permette di controllare se si tratta dell'intestazione, di righe fuori dal range o se si tratta di pezzi di testo che non c'entrano nulla
                 if is_data is True:
-                #print ("E vero?")
+                    #controllo che il valore sia un intero
                     try:
                         elements[1] = int(elements[1])
                     except: 
                         is_int = False
                 if is_int == True and is_data == True:
-                #se il valore non è nullo o negativo ed è un intero
+                    
+                    #controllo che il valore non sia negativo o nullo
                     if elements[1] > 0:
-                    #print ("Sono qui")
+
                         if elements[0] > last_date:
+                        
                         # Aggiungo alla lista gli elementi di questa linea
                             time_series.append(elements)
                             last_date = elements[0]
+                        
                         elif elements[0] == last_date:
                             raise ExamException("L'elemento {} è duplicato".format(
                             elements[0]))
+                        
                         elif elements[0] < last_date:
                             raise ExamException("L'elemento {} è fuori sequenza".format(elements[0]))
+                    
                     #altrimenti ignoro, come da consegna
 
         # Chiudo il file
@@ -247,10 +263,3 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
         risultati.append(valore)
 
     return risultati
-
-
-provina = CSVTimeSeriesFile(name="prova.csv")
-testi = provina.get_data()
-print(testi)
-results = compute_avg_monthly_difference(testi, "1949", "1951")
-print (results)
