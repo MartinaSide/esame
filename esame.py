7fcc3a2928634641c0509b56ba86784b9a7b1d13
Martina Siderini <93184296+MartinaSide@users.noreply.github.com>
2022-02-09 08:53:21 +0000

    26/28

--- esame.py
+++ esame.py
@@ -26,47 +26,46 @@ class CSVTimeSeriesFile:
         for line in my_file:
 
             # Faccio lo split di ogni linea sulla virgola
-            elements = line.split(',')
+            if "," in line:
+                elements = line.split(',')
 
-            # Con la funzione strip() tolgo il carattere di new line:
-            elements[1] = elements[1].strip()
-            #print (elements[1])
+                # Con la funzione strip() tolgo il carattere di new line:
+                elements[1] = elements[1].strip()
+                #print (elements[1])
 
-            #datum è una lista contentente la prima parte di elements, cioè la data
-            datum = [0, 0]
-            #provo a dividere sulla "-"
-            if "-" in elements[0]:
-                datum = elements[0].split('-')
-            #quando non è presente, l'elemento che sto analizzando non è una data nel formato richiesto e quindi lo ignoro o è la riga di intestazione e quindi la ignoro
-            is_data = True
-            #print (datum[0])
-            #print (datum[1])
-            datum[0] = int(datum[0])
-            datum[1] = int(datum[1])
-            if datum[0] not in range(1949, 1960):
-                is_data = False
-            if datum[1] not in range(1, 13):
-                is_data = False
-            # is_data mi permette di controllare se si tratta dell'intestazione, di righe fuori dal range o se si tratta di pezzi di testo che non c'entrano nulla
-            #print (is_data)
-            if is_data is True:
+                #datum è una lista contentente la prima parte di elements, cioè la data
+                datum = [0, 0]
+                #provo a dividere sulla "-"
+                if "-" in elements[0]:
+                    datum = elements[0].split('-')
+                #quando non è presente, l'elemento che sto analizzando non è una data nel formato richiesto e quindi lo ignoro o è la riga di intestazione e quindi la ignoro
+                is_data = True
+                #print (datum[0])
+                #print (datum[1])
+                datum[0] = int(datum[0])
+                datum[1] = int(datum[1])
+                if datum[0] not in range(1949, 1960):
+                    is_data = False
+                if datum[1] not in range(1, 13):
+                    is_data = False
+                # is_data mi permette di controllare se si tratta dell'intestazione, di righe fuori dal range o se si tratta di pezzi di testo che non c'entrano nulla
+                #print (is_data)
+                if is_data is True:
                 #print ("E vero?")
-                elements[1] = int(elements[1])
+                    elements[1] = int(elements[1])
 
                 #se il valore non è nullo o negativo ed è un intero
-                if elements[1] > 0:
+                    if elements[1] > 0:
                     #print ("Sono qui")
-                    if elements[0] > last_date:
+                        if elements[0] > last_date:
                         # Aggiungo alla lista gli elementi di questa linea
-                        time_series.append(elements)
-                        last_date = elements[0]
-                    elif elements[0] == last_date:
-                        raise ExamException("L'elemento {} è duplicato".format(
+                            time_series.append(elements)
+                            last_date = elements[0]
+                        elif elements[0] == last_date:
+                            raise ExamException("L'elemento {} è duplicato".format(
                             elements[0]))
-                    else:
-                        raise ExamException(
-                            "L'elemento {} è fuori sequenza".format(
-                                elements[0]))
+                        else:
+                            raise ExamException("L'elemento {} è fuori sequenza".format(elements[0]))
                     #altrimenti ignoro, come da consegna
 
         # Chiudo il file
@@ -131,9 +130,6 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
             indice_anno = curr_year_n_month[0] - first_year
             indice_mese = curr_year_n_month [1] - 1
             matrice_dati [indice_anno] [indice_mese] = time_series [i] [1]
-    print (matrice_dati[0])
-    print (matrice_dati [1])
-    print (matrice_dati [2])
 
     risultati = []
     precedente = 0
@@ -150,17 +146,17 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
                 differenza = attuale - precedente
                 somma = somma + differenza
                 contatore = contatore + 1
-                print ("{} - {}" .format(attuale, precedente))
+                #print ("{} - {}" .format(attuale, precedente))
             precedente = attuale
                 
         if contatore > 0:
             valore = somma / contatore
         else:
             valore = somma
-        print ("Il valore della somma nel ciclo {}" .format(i))
-        print (somma)
-        print (valore)
-        print ("Ecco i valori del ciclo seguente:")
+        #print ("Il valore della somma nel ciclo {}" .format(i))
+        #print (somma)
+        #print (valore)
+        #print ("Ecco i valori del ciclo seguente:")
         differenza = 0
         contatore = 0
         somma = 0
@@ -169,10 +165,11 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
 
     return risultati
 
-
+"""
 provina = CSVTimeSeriesFile(name="prova.csv")
 testi = provina.get_data()
 print(testi)
 results = compute_avg_monthly_difference(testi, "1949", "1951")
 print (results)
-print (len(results))
+#print (len(results))
+"""
