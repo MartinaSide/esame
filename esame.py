f0db5cf313ee75ef7165d3ee8636dbe0574959d8
Martina Siderini <93184296+MartinaSide@users.noreply.github.com>
2022-02-09 09:15:22 +0000

    27.5/28

--- esame.py
+++ esame.py
@@ -28,8 +28,12 @@ class CSVTimeSeriesFile:
             # Faccio lo split di ogni linea sulla virgola
             if "," in line:
                 elements = line.split(',')
+                is_data = True
+                is_int = True
                 if len(elements) < 2:
-                    pass
+                    #non lavoro su questi dati e li ignoro
+                    is_data = False
+                    is_int = False
                 elif len(elements) == 2:
                     # Con la funzione strip() tolgo il carattere di new line:
                     elements[1] = elements[1].strip()
@@ -38,30 +42,29 @@ class CSVTimeSeriesFile:
                     while len(elements)>2:
                         elements.pop(-1)
                     elements[1].strip()
-
-
-
+                
                 #datum è una lista contentente la prima parte di elements, cioè la data
                 datum = [0, 0]
                 #provo a dividere sulla "-"
                 if "-" in elements[0]:
                     datum = elements[0].split('-')
-                #quando non è presente, l'elemento che sto analizzando non è una data nel formato richiesto e quindi lo ignoro o è la riga di intestazione e quindi la ignoro
-                is_data = True
-                #print (datum[0])
-                #print (datum[1])
-                datum[0] = int(datum[0])
-                datum[1] = int(datum[1])
-                if datum[0] not in range(1949, 1960):
-                    is_data = False
-                if datum[1] not in range(1, 13):
-                    is_data = False
-                # is_data mi permette di controllare se si tratta dell'intestazione, di righe fuori dal range o se si tratta di pezzi di testo che non c'entrano nulla
-                #print (is_data)
+                    #quando non è presente, l'elemento che sto analizzando non è una data nel formato richiesto e quindi lo ignoro o è la riga di intestazione e quindi la ignoro
+                    is_data = True
+                    datum[0] = int(datum[0])
+                    datum[1] = int(datum[1])
+                    if datum[0] not in range(1949, 1960):
+                        is_data = False
+                    if datum[1] not in range(1, 13):
+                        is_data = False
+                    # is_data mi permette di controllare se si tratta dell'intestazione, di righe fuori dal range o se si tratta di pezzi di testo che non c'entrano nulla
+                    #print (is_data)
                 if is_data is True:
                 #print ("E vero?")
-                    elements[1] = int(elements[1])
-
+                    try:
+                        elements[1] = int(elements[1])
+                    except: 
+                        is_int = False
+                if is_int == True:
                 #se il valore non è nullo o negativo ed è un intero
                     if elements[1] > 0:
                     #print ("Sono qui")
